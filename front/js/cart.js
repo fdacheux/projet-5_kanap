b407a8b87e30942cbc85a84df5e84ed7519a28fb
Fanny Dacheux <fanny.dacheux@gmail.com>
2022-07-05 09:51:52 +0200

    feat(cart js): calculate total quantity of articles and add it to the quantity paragraph

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,41 +1,31 @@
-const apiUrl = "http://localhost:3000/api/products";
+const API_URL = "http://localhost:3000/api/products";
 
 // Get cart from localStorage :
-let cart = JSON.parse(localStorage.getItem('cart'));
+const CART = JSON.parse(localStorage.getItem('cart'));
 
 
 // Create cards for products : 
-const cartItemCard = document.getElementById('cart__items');
-const template = document.querySelector("#cart__item-card");
-const cartProductImage = document.querySelector('img');
+const CART_ITEMS_CARDS = document.getElementById('cart__items');
+const TEMPLATE = document.querySelector("#cart__item-card");
+// const cartProductImage = document.querySelector('img');
 
+// Function that creates API URL pointing to one product
 const createApiUrlForElement = (id) => {
-    let apiForElement = `${apiUrl}/${id}`;
+    let apiForElement = `${API_URL}/${id}`;
     return apiForElement;
 }
 
-const selectProductCard = (id) => {
-    let productCard = document.querySelector(`[data-id="${id}"]`);
+// Function that selects a specific card using data-id and data-colour attributes
+const selectProductCard = (id, color) => {
+    let productCard = document.querySelector(`[data-id="${id}"][data-color="${color}"]`);
     return productCard;
 }
 
 
-async function insertInfo (url) {
-    try {
-        let response = await fetch(url);
-        let product = await response.json();
-        return product
-    }
-    catch(err){
-        console.error(err.stack);
-    }
-    
-}
-
 // Create cards
 
-for (element of cart){
-    let clone = template.content.cloneNode(true);
+for (element of CART){
+    let clone = TEMPLATE.content.cloneNode(true);
     let cartItem = clone.querySelector('article');
     let quantityInput = clone.querySelector('input');
     // let totalProductParagraph = document.querySelector('.cart__item__content__description :nth-child(2)');
@@ -45,10 +35,10 @@ for (element of cart){
     quantityInput.setAttribute('value', `${element.quantity}`);
     // totalProductParagraph.textContent = `${price * quantities}`;
     
-    cartItemCard.appendChild(clone);
+    CART_ITEMS_CARDS.appendChild(clone);
 }
 
-// Fetch url for each product
+// Fetch url for each product (!!! send promise => need to be solved)
 
 async function insertElementProperties (url){
     try {
@@ -62,24 +52,65 @@ async function insertElementProperties (url){
 }
 
 // Insert properties in each card
+const calculatePrice = (price, quantity) => {
+    return price*quantity
+}
 
-for (element of cart){
-    let id = element.id; //get id
-    let selectedCard = selectProductCard(id); //get card using data-id attribute
-    let url = createApiUrlForElement(id); // create the url to fecth right data in API
-    let product = insertElementProperties(url); // fetch api, get product properties
+
+const addAllTotals = (total, x) => {
+    total += x;
+    return total
+}
+
+var total = 0; 
+
+for (element of CART){
+
+    // Get elements from CART array
+
+    let id = element.id; //get id of element
+    let color = element.color; // get colour of element
+    let quantities = element.quantity; // get quantity of element 
+    let totalPrice;
+    let total = 0;
+    let selectedCard = selectProductCard(id, color); //get card using data-id end data-color attributes
+    let url = createApiUrlForElement(id); // create the url to fecth the right data in API
+    
 
     // Get the elements that need to be filled with API data
 
     let cartProductImage = selectedCard.querySelector('img'); //get image
-
-
+    let itemContentDescription = selectedCard.querySelector('.cart__item__content__description');// Get parent element for description
+    let productName = itemContentDescription.querySelector('h2'); //Name
+    let productColor = itemContentDescription.querySelector(':nth-child(2)');//colour 
+    let priceParagraph = itemContentDescription.querySelector(':nth-child(3)');//price (= price*quantity)
+    // let totalParagraph = document.getElementById('totalPrice');
+    
     // Set attributes and/or text for each property of the selected product
-    product.then((product) => cartProductImage.setAttribute('src', `${product.imageUrl}`));
-        
+
+    productColor.textContent = element.color; //Colour
+    insertElementProperties(url) // fetch api, get product properties
+        .then(function(res) { // set attributes of elements
+            cartProductImage.setAttribute('src', `${res.imageUrl}`); //Image URL
+            productName.textContent = res.name; // Name of product
+            totalPrice = calculatePrice(res.price, quantities);
+            console.log(totalPrice);
+            priceParagraph.textContent = `${totalPrice} €` // Total price for the product 
+        });
+    
 }
 
-// Put right elements in it 
+// Calculate total price and quantity :
 
-let cartItemCardsObject = document.querySelectorAll('article');
+    
+document.getElementById('totalPrice').textContent = `${total}`;
 
+let quantitiesArray = [];
+
+for (element of CART){
+    quantitiesArray.push(element.quantity);
+}
+
+const totalQuantity = quantitiesArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+document.getElementById('totalQuantity').textContent = totalQuantity;
+console.log(totalQuantity); 
